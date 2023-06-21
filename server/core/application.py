4b6f5c7b5892937057fdd78267e943a7663b2ea4
chandan <chandancse.iitgn@gmail.com>
2023-06-21 16:53:04 +0530

    added orgin *

--- server/core/application.py
+++ server/core/application.py
@@ -15,7 +15,7 @@ def create_app():
     app.include_router(comment.router)
     app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=['*'],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"],
